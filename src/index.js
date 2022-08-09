2cd18b1fb8b5b1bc211225167597e24079842f39
Jeyi Adole <erthnice@gmail.com>
2022-08-09 08:26:05 +0100

    Build with react-scripts to run.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Making five changes for react to run build.
+// 1. React scripts install.
+// 2. Replace dev script to 'react-scripts start' from 'vite'.
+// 3. Take index.html into the public folder which will be created by react scripts upon installation.
+// 4. Change index.jsx extension to index.js.
+// 5. Change App import in index.js to app.jsx.
+// 6. Remove browser defaults
